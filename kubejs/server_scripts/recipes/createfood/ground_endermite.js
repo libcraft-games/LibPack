5ebe471ad4f2c0bd18abaeaa1eb3934c1df95cca
dninemfive <11084010+dninemfive@users.noreply.github.com>
2025-11-12 10:10:06 -0700

    ground_endermite

---
+++ kubejs/server_scripts/recipes/createfood/ground_endermite.js
@@ -0,0 +1,20 @@
+let file = "recipes/create/rose_quartz.js";
+
+let ground_endermite = 'createfood:ground_endermite';
+let mite_crust = 'endersdelight:mite_crust';
+
+ServerEvents.recipes(e => {
+    e.shapeless(
+        Item.of(ground_endermite),
+        [
+            mite_crust
+        ]
+    );
+    e.custom({
+        type: 'create:pressing',
+        ingredients: [{item: mite_crust}],
+        results: [{id: ground_endermite, count: 2}]
+    });
+
+    console.log(`applied changes from ${file}`);
+});
