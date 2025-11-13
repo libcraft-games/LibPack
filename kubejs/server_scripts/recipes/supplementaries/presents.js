5d63317ba7b4e6b1bfd4833b4179dfb22d79622d
dninemfive <11084010+dninemfive@users.noreply.github.com>
2025-11-13 15:25:37 -0700

    presents

---
+++ kubejs/server_scripts/recipes/supplementaries/presents.js
@@ -0,0 +1,27 @@
+ServerEvents.recipes(e => {
+    let file = `recipes/supplementaries/presents.js`
+    
+    let present = 'supplementaries:present'
+    let cardboard = 'create:cardboard'
+
+    e.remove({output: present, not: {input: cardboard}})
+
+    global.libcraft.colors.forEach(color => {
+        let dye = `minecraft:${color}_dye`
+        
+        e.shaped(
+            `supplementaries:present_${color}`,
+            [
+                'CCC',
+                'CDC',
+                'CCC'
+            ], 
+            {
+                C: cardboard,
+                D: dye
+            }
+        )
+    })
+
+    console.log(`applied changes from ${file}`);
+});
