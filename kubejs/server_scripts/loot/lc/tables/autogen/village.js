8ca1b6669fda78c3c25daf8ddf31042d62a782e4
dninemfive <11084010+dninemfive@users.noreply.github.com>
2025-12-12 12:02:10 -0700

    methinks i am overdoing this

--- kubejs/server_scripts/loot/lc/tables/autogen/village.js
+++ kubejs/server_scripts/loot/lc/tables/autogen/village.js
@@ -1,52 +1,56 @@
-// creates a subtable with a fixed maximum count for simple things like logs and crops
+// creates a group with a fixed maximum count per item
 //
-// e                LootTableEventJS    the event to create tables in
-// village_name     string              the name of the village type (e.g. desert)
-// table_name       string              the name of the table (e.g. crops)
 // items            ResourceLocation[]  the registry names of the items to add to the table
 // max_ct           int                 the maximum count of each item
 //
-// returns: the path to the newly-added table            
-function create_subtable_fixed_ct(e, village_name, table_name, items, max_ct) {
-    let table_path = `lc:tables/autogen/village/${village_name}/${table_name}`
-    e.create(table_path)
-     .createPool(pool => {
-        for(let item of items) {
-            pool.addEntry(LootEntry.of(item, [1, max_ct]))
-        }
-     })
-    return table_path
+// returns: the newly-created group.           
+function create_group_fixed_ct(items, max_ct) {
+    let entries = []
+    for(let item of items) {
+        entries.add(LootEntry.of(item, [1, max_ct]))
+    }
+    return LootEntry.group(entries)
 }
 // creates a subtable where each item may have a different maximum count
 //
-// e                LootTableEventJS    the event to create tables in
-// village_name     string              the name of the village type (e.g. desert)
-// table_name       string              the name of the table (e.g. crops)
 // items            (RL, int)[]         the registry names of the items to add to the table, paired with their corresponding maximum counts
 //
-// returns: the path to the newly-added table            
-function create_subtable(e, village_name, table_name, items) {
-    let table_path = `lc:tables/autogen/village/${village_name}/${table_name}`
-    e.create(table_path)
-     .createPool(pool => {
-        for(let [item, ct] of items) {
-            pool.addEntry(LootEntry.of(item, [1, ct]))
-        }
-     })
-    return table_path
+// returns: the newly-created group.
+function create_group(items) {
+    let entries = []
+    for(let [item, ct] of items) {
+        entries.add(LootEntry.of(item, [1, ct]))
+    }
+    return LootEntry.group(entries)
 }
-function create_colorful_subtable(e, village_name, colors) {
+function create_subtreasure_subtable(e, village_name, colors, tools) {
     let table_path = `lc:tables/autogen/village/${village_name}/colorful`
     e.create(table_path)
      .createPool(pool => {
         let chalks = [], sacks = [LootEntry.of('supplementaries:sack')], bundles = [LootEntry.of('minecraft:bundle').withWeight(2)]
         for(let color of colors) {
-            chalks.add(LootEntry.of(`chalk:${color}_chalk`).withWeight(3))
-            sacks.add(LootEntry.of(`supplementaries:sack_${color}`))
-            bundles.add(LootEntry.of(`minecraft:${color}_bundle`).withWeight(2))
+            chalks.add(LootEntry.of(`chalk:${color}_chalk`).withWeight(30))
+            bundles.add(LootEntry.of(`minecraft:${color}_bundle`).withWeight(20))
+            sacks.add(LootEntry.of(`supplementaries:sack_${color}`.withWeight(10)))
         }
+        pool.addEntry(LootEntry.group(chalks))
         pool.addEntry(LootEntry.group(sacks))
         pool.addEntry(LootEntry.group(bundles))
+        let tools = []
+        for(let [tool, weight] of tools) {
+            tools.add(LootEntry.of(tool).damage([0.3, 0.6]).withWeight(weight))
+        }
+        pool.addEntry(LootEntry.group(tools))
+     })
+    return table_path
+}
+function create_artifacts_subtable(e, village_name, artifacts) {
+    let table_path = `lc:tables/autogen/village/${village_name}/artifacts`
+    e.create(table_path)
+     .createPool(pool => {
+        for(let [artifact, weight] of artifacts) {
+            pool.addEntry(LootEntry.of(artifact).withWeight(weight))
+        }
      })
     return table_path
 }
@@ -58,29 +62,26 @@ function create_colorful_subtable(e, village_name, colors) {
 // crops            ResourceLocation[]  seeds and products local to the village
 // flora            (RL, int)[]         flora local to the village and their max count
 // food             (RL, int)[]         cooked food, like bread, mushroom stew, pumpkin pie
-// materials        (RL, int)[]         metal nuggets, maybe tool materials (e.g. flint)
 // products         (RL, int)[]         local products, like clay or green dye
-// trees            ResourceLocation[]  logs and saplings local to the village
 // colors           string[]            colors the sacks and bundles should be
-// artifacts        (RL, int)[]         artifacts which may spawn in the village
+// artifacts        (RL, int)[]         artifacts which may spawn in the village and their weights
+// tools            (RL, int)[]         tools which may spawn in the village and their weights
 //
 // returns:         MutableLootTable    the modified table, which can have more stuff added (e.g. photographs)
-function assembleVillageLoot(e, table, name, crops, flora, food, materials, products, trees, colors, artifacts) {
+function assembleVillageLoot(e, table, name, crops, flora, food, products, colors, artifacts, tools) {
     table.clear()
     table.createPool(pool => {
         pool.rolls(3, 8)
-        pool.addEntry(LootEntry.reference(create_subtable_fixed_ct(e, name, 'crops', crops, 5)))
-        pool.addEntry(LootEntry.reference(create_subtable_fixed_ct(e, name, 'trees', trees, 3)))
-        pool.addEntry(LootEntry.reference(create_subtable(e, name, 'flora', flora)))
-        pool.addEntry(LootEntry.reference(create_subtable(e, name, 'food', food)))
-        pool.addEntry(LootEntry.reference(create_subtable(e, name, 'materials', materials)))
-        pool.addEntry(LootEntry.reference(create_subtable(e, name, 'products', products)))
+        pool.addEntry(create_group_fixed_ct(crops, 5))
+        pool.addEntry(create_group(flora))
+        pool.addEntry(create_group(food))
+        pool.addEntry(create_group(products))
         pool.addEntry(LootEntry.of('minecraft:emerald', [1, 4]))
     })
     table.createPool(pool => {
-        pool.addEntry(LootEntry.empty().withWeight(12))
-        pool.addEntry(LootEntry.reference(create_colorful_subtable(e, name, colors)).withWeight(5))
-        pool.addEntry(LootEntry.reference(create_subtable(e, name, 'artifacts', artifacts)))
+        pool.addEntry(LootEntry.empty()                                                         .withWeight(12))
+        pool.addEntry(LootEntry.reference(create_subtreasure_subtable(e, name, colors, tools))  .withWeight(5))
+        pool.addEntry(LootEntry.reference(create_artifacts_subtable(e, artifacts))              .withWeight(1))
     })
     return table
 }
---
+++ kubejs/server_scripts/loot/minecraft/chests/village/village_plains_house.js
@@ -0,0 +1,33 @@
+LootJS.lootTables(e => {
+    let mc = 'minecraft'
+    let fd = 'farmersdelight'
+    let af = 'artifacts'
+    // e                LootTableEventJS    the event to create tables in
+    // table            MutableLootTable    the parent table to modify
+    // name             string              the village name used to create identifiers
+    // crops            ResourceLocation[]  seeds and products local to the village
+    // flora            (RL, int)[]         flora local to the village and their max count
+    // food             (RL, int)[]         cooked food, like bread, mushroom stew, pumpkin pie
+    // products         (RL, int)[]         local products, like clay or green dye
+    // colors           string[]            colors the sacks and bundles should be
+    // artifacts        (RL, int)[]         artifacts which may spawn in the village and their weights
+    // tools            (RL, int)[]         tools which may spawn in the village and their weights
+    assembleVillageLoot(e, e.getLootTable('minecraft:chests/village/village_plains_house'),
+        'plains',
+        [`${mc}:carrot`, `${fd}:onion`, `${vd}:dandelion_leaf`, `${mc}:wheat`, `${vd}:cauliflower`, `${vd}:garlic`, `${ie}:seed`],
+        [[`${mc}:dandelion`,    5], 
+         [`${mc}:poppy`,        2], 
+         [`${mc}:cornflower`,   1], [`${mc}:azure_bluet`, 1], [`${mc}:oxeye_daisy`, 1],
+         [`${mc}:oak_sapling`,  3]],
+        [[`${mc}:bread`, 6], [`${mc}:cooked_beef`, 2], [`${mc}:cooked_porkchop`, 3], [`${mc}:cooked_mutton`, 4]], // todo: more
+        [[`supplementaries:rope`, 8], [`${mc}:lead`, 3]], // todo: products
+        ['green', 'blue', 'brown', 'light_blue'],
+        [[`${af}:everlasting_beef`, 4], [`${af}:eternal_steak`, 1], 
+         [`${af}:villager_hat`, 5], 
+         [`${af}:superstitious_hat`, 2], 
+         [`${af}:cowboy_hat`, 2], 
+         [`${af}:lucky_scarf`, 1],
+         [`${af}:running_shoes`, 1]],
+        [[`${mc}:shears`, 2], [`${mc}:stone_hoe`, 1]]
+    )
+})
