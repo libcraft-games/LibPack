e4e82c41013b4f71a1cad8e77586340f7b7cfb43
dninemfive <11084010+dninemfive@users.noreply.github.com>
2025-12-13 13:32:34 -0700

    0.7.37-pre.23 feat: cheaper recipes for Create rose quartz using Redstone Acid (resolves #189)

--- kubejs/server_scripts/recipes/create/rose_quartz.js
+++ kubejs/server_scripts/recipes/create/rose_quartz.js
@@ -1,10 +1,8 @@
-
-
-let create_rose_quartz = 'create:rose_quartz';
-let bop_rose_quartz = 'biomesoplenty:rose_quartz_chunk';
-let redstone = '#c:dusts/redstone';
-
 ServerEvents.recipes(e => {
+    let ie = 'immersiveengineering'
+    let create_rose_quartz = 'create:rose_quartz'
+    let bop_rose_quartz = 'biomesoplenty:rose_quartz_chunk'
+    let redstone = '#c:dusts/redstone'
     e.shapeless(
         Item.of(create_rose_quartz),
         [
@@ -12,6 +10,41 @@ ServerEvents.recipes(e => {
             `3x ${redstone}`
         ]
     )
-
+    e.custom({
+        "type": "create:mixing",
+        "ingredients": [
+            {
+                "item": 'minecraft:quartz'
+            },
+            {
+                type: "neoforge:single",
+                amount: 1000,
+                fluid: `${ie}:redstone_acid`
+            }
+        ],
+        "results": [
+            {
+                id: create_rose_quartz
+            }
+        ]
+    })
+    e.custom({
+        "type": "create:mixing",
+        "ingredients": [
+            {
+                "item": bop_rose_quartz
+            },
+            {
+                type: "neoforge:single",
+                amount: 375,
+                fluid: `${ie}:redstone_acid`
+            }
+        ],
+        "results": [
+            {
+                id: create_rose_quartz
+            }
+        ]
+    })
     console.log(`applied all changes successfully!`);
 });
