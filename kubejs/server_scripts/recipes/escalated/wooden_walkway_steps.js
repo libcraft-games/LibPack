31b53b6ce832eb691777b0b33dfee1a03d7df09c
dninemfive <11084010+dninemfive@users.noreply.github.com>
2025-12-19 02:01:14 -0700

    chore(recipe): must treat wooden walkways (resolves #265)

---
+++ kubejs/server_scripts/recipes/escalated/wooden_walkway_steps.js
@@ -0,0 +1,7 @@
+ServerEvents.recipes(e => {
+    e.replaceInput(
+        {output: 'escalated:wooden_walkway_steps'},
+        '#minecraft:wooden_slabs',
+        '#immersiveengineering:treated_wood_slab'
+    )
+})
