81a2477d59b64541b13c7cd048a2063b850ec0a9
dninemfive <11084010+dninemfive@users.noreply.github.com>
2026-01-13 13:04:14 -0700

    chore(loot): Supplementaries galleon safe loot table (#77)

--- kubejs/server_scripts/loot/lc/tables/structure/minecraft/shipwreck/treasure/artifacts.js
+++ kubejs/server_scripts/loot/lc/tables/structure/minecraft/shipwreck/treasure/artifacts.js
@@ -5,6 +5,5 @@ LootJS.lootTables(e => {
         pool.addEntry(LootEntry.of(`${a}:golden_hook`).withWeight(4))
         pool.addEntry(LootEntry.of(`${a}:charm_of_sinking`).withWeight(3))
         pool.addEntry(LootEntry.of(`${a}:anglers_hat`).withWeight(2))
-        pool.addEntry(LootEntry.of(`${a}:aqua_dashers`).withWeight(1))
     })
 })
---
+++ kubejs/server_scripts/loot/lc/tables/structure/supplementaries/galleon/safe/misc.js
@@ -0,0 +1,39 @@
+LootJS.lootTables(e => {
+    let a = 'artifacts'
+    let mc = 'minecraft'
+    let sa = 'sweety_archaeology'
+    let su = 'supplementaries'
+    let table = e.create(`lc:tables/structure/${su}/galleon/safe/misc`)
+    table.createPool(pool => {
+        pool.addEntry(LootEntry.of(`${mc}:map`, [1, 2]).withWeight(24))
+        pool.addEntry(LootEntry.of(`${su}:blast_armor_trim_smithing_template`).withWeight(16))
+        pool.addEntry(LootEntry.of(`${su}:antique_ink`, [1, 2]).withWeight(16))
+        pool.addEntry(LootEntry.of(`${su}:antique_ink`, [1, 2]).withWeight(16))
+        pool.addEntry(LootEntry.of(`${mc}:map`).withWeight(12)
+            .jsonFunction({
+                function: `${mc}:exploration_map`,
+                decoration: `${mc}:red_x`,
+                skip_existing_chunks: false,
+                zoom: 1
+            })
+            .jsonFunction({
+                function: `${mc}:set_name`,
+                name: {
+                    translate: "filled_map.buried_treasure"
+                },
+                target: "item_name"
+            }))
+        pool.addEntry(LootEntry.of(`${su}:bomb_blue`, [2, 4]).withWeight(12))
+        pool.addEntry(LootEntry.of(`${mc}:fire_charge`, [1, 3]).withWeight(8))
+        pool.addEntry(LootEntry.of(`${su}:globe`).withWeight(8))
+        pool.addEntry(LootEntry.of(`${su}:hourglass`).withWeight(8))
+        // todo: stick a parrot in here
+        pool.addEntry(LootEntry.of(`${su}:cage`).withWeight(6))
+        pool.addEntry(LootEntry.of(`${mc}:spyglass`).withWeight(6))
+        pool.addEntry(LootEntry.of(`${mc}:compass`).withWeight(6))
+        pool.addEntry(LootEntry.of(`${a}:golden_hook`).withWeight(6))
+        pool.addEntry(LootEntry.of(`${a}:anglers_hat`).withWeight(3))
+        pool.addEntry(LootEntry.of(`${sa}:golden_cube`).withWeight(2))
+        pool.addEntry(LootEntry.of(`${su}:music_disc_heave_ho`).withWeight(1))
+    })
+})
