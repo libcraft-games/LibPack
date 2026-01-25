3834d58cb4b7ab863e6bcccaf80dfb9c816322c8
dninemfive <11084010+dninemfive@users.noreply.github.com>
2026-01-25 10:21:35 -0700

    misc: delete that file i just added

--- kubejs/server_scripts/loot/lc/tables/general/scriptor/artifacts.js
+++
@@ -1,8 +0,0 @@
-LootJS.lootTables(e => {
-    return
-    e.create('lc:tables/general/scriptor/artifacts').createPool(pool => {
-        pool.addEntry(LootEntry.of('scriptor:artifact_1').jsonFunction({
-            function: 'scriptor:artifact'
-        }))
-    })
-})
