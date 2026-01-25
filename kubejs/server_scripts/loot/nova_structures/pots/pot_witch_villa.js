279ec82c19e8db291309e877e17b446f19d9901c
dninemfive <11084010+dninemfive@users.noreply.github.com>
2026-01-25 10:15:26 -0700

    chore(loot): adjust witch villa pot loot table (#77) refactor(loot): add unified method for creating Nova Structures pot loot tables

--- kubejs/server_scripts/loot/nova_structures/pots/pot_witch_villa.js
+++ kubejs/server_scripts/loot/nova_structures/pots/pot_witch_villa.js
@@ -1,26 +1,29 @@
 LootJS.lootTables(e => {
+    let bp = 'biomesoplenty'
     let ew = 'enchanted'
+    let fd = 'farmersdelight'
     let ie = global.ie.namespace
     let mc = 'minecraft'
     let su = 'supplementaries'
-    let table = e.getLootTable('nova_structures:pots/pot_witch_villa')
-    table.clear()
-    table.createPool(pool => {
-        let items = [
-            [`${mc}:sugar`, 8],
-            [`${mc}:spider_eye`, 7],
-            [`${mc}:slime_ball`, 6],
-            [`${mc}:emerald`, 5],
-            [`${ew}:belladonna_flower`, 4],
-            [`${mc}:rabbit_foot`, 3],
-            [`${mc}:redstone`, 2],
-            [`${mc}:glowstone_dust`, 1]
-        ]
-        let wt = 0
-        for(let [item, weight] of items) {
-            pool.addEntry(LootEntry.of(item, [3, 7]).withWeight(weight))
-            wt += weight
-        }
-        pool.addEntry(LootEntry.empty().withWeight(Math.floor(wt * 0.15)))
-    })
+    
+    let items = [
+        [`${fd}:rice_panicle`, 9],
+        [`${ew}:wolfsbane_flower`, 7],
+        [`${ew}:belladonna_flower`, 6],
+        [`${mc}:sugar_cane`, 6],
+        [`${bp}:reed`, 5],
+        [`${bp}:cattail`, 5],
+        [`${bp}:watergrass`, 5],
+        [`${ew}:water_artichoke`, 5],
+        [`${ew}:mandrake_root`, 5],
+        [`${mc}:slime_ball`, 5],
+        [`${mc}:sugar`, 5],
+        [`${mc}:fermented_spider_eye`, 5],
+        [`${ew}:tongue_of_dog`, 3],
+        [`${ew}:wool_of_bat`, 3],
+        [`${mc}:emerald`, 3],
+        [`${mc}:redstone`, 2],
+        [`${mc}:glowstone_dust`, 1]
+    ]
+    GeneratePotPool(e.getLootTable('nova_structures:pots/pot_witch_villa'), items)
 })
