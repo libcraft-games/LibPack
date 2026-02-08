9a8201423024404773d14b95442ae160dfa8fdae
dninemfive <11084010+dninemfive@users.noreply.github.com>
2026-02-08 12:25:17 -0700

    feat(recipe): rest of the CNA recipes, i think (#297)

---
+++ kubejs/server_scripts/recipes/create_new_age/heater.js
@@ -0,0 +1,20 @@
+ServerEvents.recipes(e => {
+    let cn = 'create_new_age'
+    let ie = global.ie.namespace
+
+    let item = `${cn}:heater`
+    e.remove({output: item})
+    e.shaped(
+        Item.of(item),
+        [
+            'ABA',
+            'CDC'
+        ],
+        {
+            A: `#c:ingots/nickel`,
+            B: `#c:sheetmetals/constantan`,
+            C: `${cn}:heat_pipe`,
+            D: `${cn}:heat_pump`,
+        }
+    )
+})
